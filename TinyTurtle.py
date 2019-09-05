fe1a715ffc60b2283cbe70a87091e780f6c3a018
Abereni Opuiyo <ajo5902@g.rit.edu>
2019-09-05 01:55:38 -0400

    Added functions for moving the turtle marker forward and backward

--- TinyTurtle.py
+++ TinyTurtle.py
@@ -6,3 +6,21 @@ Turtle interpreter!
 """
 
 import turtle as tt
+
+def cmdF(value):
+    """
+    cmdF moves the turtle marker forward a certain amount.
+    :param value: How far you want the turtle to move.
+    :return:
+    """
+    tt.fd(value)
+
+def cmdB(value):
+    """
+    cmdB moves the turtle marker backward a certain amount.
+
+    :param value: the distance the turtle marker moves backward
+    :return:
+    """
+    tt.back(value)
+    
