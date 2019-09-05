45847401450a99d32d52e5b7ff5076df18e7037c
Abereni Opuiyo <ajo5902@g.rit.edu>
2019-09-05 18:17:59 -0400

    Added expander functionality, and completed all TT commands

--- TinyTurtle.py
+++ TinyTurtle.py
@@ -223,6 +223,101 @@ def cmdI(amount, command):
         loops -= 1
         command = commandcpy
 
+
+def commandExpander(cmd):
+    commandSnip = ""
+    while cmd != "":
+        if cmd[0] == "F":
+            nextcmd = valLength(cmd[1:]) + 2
+            print(cmd[0] + str(getValue(cmd[1:])), end=" ")
+            cmd = cmd[nextcmd:]
+
+        elif cmd[0] == "B":
+            nextcmd = valLength(cmd[1:]) + 2
+            print(cmd[0] + str(getValue(cmd[1:])), end=" ")
+            cmd = cmd[nextcmd:]
+
+        elif cmd[0] == "L":
+            nextcmd = valLength(cmd[1:]) + 2
+            print(cmd[0] + str(getValue(cmd[1:])), end=" ")
+            cmd = cmd[nextcmd:]
+
+        elif cmd[0] == "R":
+            nextcmd = valLength(cmd[1:]) + 2
+            print(cmd[0] + str(getValue(cmd[1:])), end=" ")
+            cmd = cmd[nextcmd:]
+
+        elif cmd[0] == "C":
+            nextcmd = valLength(cmd[1:]) + 2
+            print(cmd[0] + str(getValue(cmd[1:])), end=" ")
+            cmd = cmd[nextcmd:]
+
+        elif cmd[0] == "U":
+            print(cmd[0], end=" ")
+            cmd = cmd[2:]
+
+        elif cmd[0] == "D":
+            print(cmd[0], end=" ")
+            cmd = cmd[2:]
+
+        elif cmd[0] == "I":
+            loops = getValue(cmd[1:])
+
+            cmd1 = valLength(cmd[1:]) + 2
+            cmd1C = cmd[cmd1]
+            cmd1Val = getValue(cmd[cmd1+1:])
+            cmd1ValOrig = str(cmd1Val)
+            secCmdLoc = valLength(str(cmd1Val))
+
+            if cmd[cmd1 + 1] == "0":
+                secCmdLoc += 1
+                cmd1ValOrig = "0" + str(cmd1Val)
+
+            cmd2 = cmd1 + secCmdLoc + 2
+            cmd2C = cmd[cmd2]
+            cmd2Val = getValue(cmd[cmd2+1:])
+            cmd2ValOrig = str(cmd2Val)
+
+            if cmd[cmd2 +1] == "0":
+                cmd2 += 1
+                cmd2ValOrig = "0" + str(cmd2Val)
+
+            cmd1Merged = cmd1C + cmd1ValOrig
+            cmd2Merged = cmd2C + cmd2ValOrig
+
+            nextcmd = cmd1 + cmd2 + valLength(str(cmd2Val)) + 1
+
+            while loops != 0:
+                print(cmd1Merged, cmd2Merged, end=" ")
+                loops -= 1
+
+            cmd = cmd[nextcmd:]
+
+        elif cmd[0] == "P":
+            sides = getValue(cmd[1:])
+            angle = int(360 / sides)
+
+            angleSTR = str(angle)
+            length = getValue(cmd[valLength(cmd[1:]) + 2:])
+
+            if cmd[valLength(cmd[1:]) + 2] == "0":
+                length = "0" + str(length)
+
+            if len(angleSTR) < 3:
+                angleSTR = "0" + angleSTR
+
+            cmdLength1 = valLength(cmd[1:]) + 1
+            cmdLength2 = valLength(cmd[cmdLength1 + 1:]) + 2
+
+            nextcmd = cmdLength1 + cmdLength2
+
+            while sides != 0:
+                print("F", length, sep="", end=" ")
+                print("L", angleSTR, sep="", end=" ")
+                sides -= 1
+            cmd = cmd[nextcmd:]
+
+
 def tinyTurtle(cmd):
     idx = 0
     length = len(cmd) - 1
@@ -295,6 +390,11 @@ def tinyTurtle(cmd):
                 break
 
 
+#commandExpander("P4 050")
+
+
+commandExpander("P3 100 U F200 D P4 050 U F100 D P8 025 U B500 D I2 C050 L180 @")
+
 #processArg("I", "2 F100 C10 F100 U D P3 100 @")
 
 
@@ -302,7 +402,7 @@ def tinyTurtle(cmd):
 
 # = "cool beans"
 
-tinyTurtle("I2 I4 F100 L090 @ F100 @")
+#tinyTurtle("I2 I4 F100 L090 @ F100 @")
 
 
 #idx = 1
